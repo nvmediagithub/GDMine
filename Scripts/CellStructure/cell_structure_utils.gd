ac33d639e334e21fe3f462d8047dda95f97abba4
Nikita <103855729+nvmediagithub@users.noreply.github.com>
2025-02-23 17:59:36 +0300

    fix

---
+++ Scripts/CellStructure/cell_structure_utils.gd
@@ -0,0 +1,84 @@
+# cell_structure_utils.gd
+extends RefCounted
+class_name CellStructureUtils
+
+static func calculate_angle(start: Vector2, end: Vector2) -> float:
+	# Вычисление угла в радианах
+	var dx: float = end.x - start.x
+	var dy: float = end.y - start.y
+	return atan2(dy, dx)
+
+static func generate_child_rays(
+		start_point: CellPoint, 
+		base_direction: float, 
+		child_count: int = 2,
+		min_length: float = 1, 
+		max_length: float = 2,
+		max_deviation: float = PI
+	) -> Array[CellPoint]:
+	"""
+	Генерирует child_count лучей из start_point.
+	Направление каждого луча основывается на base_direction (в радианах) с отклонением не более max_deviation.
+
+	Возвращает список лучей, где каждый луч представлен как список [start_point, end_point],
+	а end_point – объект CellPoint с целочисленными координатами.
+	"""
+	if child_count == 0:
+		return []
+	var points: Array[CellPoint] = []
+	var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+	var base_step: float = max_deviation * 2 / child_count
+	var deviation_limit: float = base_step / 4
+	for n: int in range(child_count):
+		# Вычисляем случайное отклонение
+		var deviation: float =\
+			rng.randf_range(
+					-max_deviation + base_step * n + deviation_limit, 
+					-max_deviation + base_step * (n+1) - deviation_limit
+				)
+		var new_angle: float = base_direction + deviation
+		# Выбираем случайную длину луча
+		var length: float = rng.randf_range(min_length, max_length)
+		var dx: float  = cos(new_angle) * length
+		var dy: float = sin(new_angle) * length
+		# Вычисляем координаты конечной точки и округляем до целых чисел
+		var new_x: float = start_point.position.x + dx
+		var new_y: float = start_point.position.y + dy
+		var end_point: CellPoint = CellPoint.new(Vector2(new_x, new_y))
+		points.append(end_point)
+	return points
+
+
+static func line_intersection(p1: CellPoint, p2: CellPoint, p3: CellPoint, p4: CellPoint, tol: float = 1e-6) -> CellPoint:
+	"""
+	Вычисляет точку пересечения двух отрезков (p1, p2) и (p3, p4) с учетом допуска tol.
+	p1, p2, p3, p4 – объекты Vector2, представляющие концы отрезков.
+	Возвращает Vector2 с координатами точки пересечения, если отрезки пересекаются, иначе null.
+	"""
+
+	if p1 == p3 or p1 == p4:
+		return null
+		
+	# Направления отрезков
+	var d1: Vector2 = p2.position - p1.position
+	var d2: Vector2 = p4.position - p3.position
+		
+	# Определитель
+	var det: float = d1.x * d2.y - d1.y * d2.x
+	# Если определитель близок к нулю, отрезки параллельны или совпадают
+	if abs(det) < tol:
+		return null
+	# Вычисление параметра t для точки пересечения
+	var diff: Vector2 = p3.position - p1.position
+	var t: float = (diff.x * d2.y - diff.y * d2.x) / det
+	# Проверка, что точка пересечения находится на первом отрезке
+	if t < 0 or t > 1:
+		return null
+	# Вычисление параметра u для точки пересечения
+	var u: float = (diff.x * d1.y - diff.y * d1.x) / det
+	# Проверка, что точка пересечения находится на втором отрезке
+	if u < 0 or u > 1:
+		return null
+	# Вычисление координат точки пересечения
+	var intersection: Vector2 = p1.position + t * d1
+	return CellPoint.new(intersection)
--- Scripts/CellStructure/chunk_manager_3d.gd
+++ Scripts/CellStructure/chunk_manager_3d.gd
@@ -4,14 +4,22 @@ class_name ChunkManager3D
 # Параметры сетки чанков
 @export var origin: Vector2i = Vector2i(0,0)
 @export var chunk_size: Vector3 = Vector3(8.0,8.0,8.0)
-@export var min_ray_lenght: float = 1.0
-@export var max_ray_lenght: float = 2.0
-var chunks: Dictionary = {}  # ключ: Vector2i, значение: Chunk3D узел
+@export var min_ray_length: float = 1.0
+@export var max_ray_length: float = 1.5
+var chunks: Dictionary = {}
+var limit: float = 0.1 
 	
 func _ready() -> void:
 	var start_point: CellPoint = CellPoint.new(Vector2(chunk_size.x,chunk_size.z) / 2)
 	load_chunk(Vector2i(0,0))
-	var points: Array[CellPoint] = generate_child_rays(start_point, 0, 6, min_ray_lenght, max_ray_lenght)
+	var points: Array[CellPoint] =\
+		CellStructureUtils.generate_child_rays(
+			start_point, 
+			0, 
+			6, 
+			min_ray_length, 
+			max_ray_length
+		)
 	var chunk: Chunk3D = chunks[Vector2i(0,0)]
 	chunk.size = chunk_size
 	for end_point: CellPoint in points:
@@ -20,12 +28,10 @@ func _ready() -> void:
 		expand_structure()
 	
 func get_chunk_key_for_point(point: Vector2) -> Vector2i:
-	#print('point ', point)
-	var relative_pos: Vector2i = Vector2i(floor(point.x / chunk_size.x), floor(point.y / chunk_size.y)) - origin
-	#print('relative_pos ', relative_pos)
-	var i: int = int(floor(relative_pos.x))
-	var j: int = int(floor(relative_pos.y))
-	return Vector2i(i, j)
+	return Vector2i(
+		floor((point.x - origin.x) / chunk_size.x), 
+		floor((point.y - origin.y) / chunk_size.z)
+	)
 
 func get_neighbor_keys(key: Vector2i) -> Array[Vector2i]:
 	var neighbors: Array[Vector2i] = []
@@ -55,7 +61,8 @@ func load_chunk(key: Vector2i) -> void:
 			neighbor_chunk.size = chunk_size
 			neighbor_chunk.set_chunk(new_slice)
 			var local_position: Vector2 = nkey * chunk_size.x
-			neighbor_chunk.position = Vector3(local_position.x, 0, local_position.y)
+			neighbor_chunk.position =\
+				Vector3(local_position.x, 0, local_position.y)
 			add_child(neighbor_chunk)
 			chunks[nkey] = neighbor_chunk
 
@@ -94,14 +101,40 @@ func expand_structure() -> void:
 			if p_end.has_emitted:
 				continue
 			p_end.has_emitted = true
-			var base_direction: float = calculate_angle(p_start.position, p_end.position)
-			var target_points: Array[CellPoint] = generate_child_rays(p_end, base_direction, 2, min_ray_lenght, max_ray_lenght, PI / 2)
+			var base_direction: float =\
+				CellStructureUtils.calculate_angle(
+					p_start.position, 
+					p_end.position
+				)
+			var target_points: Array[CellPoint] =\
+				CellStructureUtils.generate_child_rays(
+					p_end, 
+					base_direction, 
+					2, 
+					min_ray_length, 
+					max_ray_length, 
+					PI / 2
+				)
 			for target_point: CellPoint in target_points:
 				for line: CellLine in chunk.get_lines():
-					var inter: CellPoint = line_intersection(p_end.position, target_point.position, line.start.position, line.end.position)
+					var inter: CellPoint =\
+						CellStructureUtils.line_intersection(
+							p_end, 
+							target_point, 
+							line.start, 
+							line.end
+						)
 					if inter != null:
-						target_point = inter
-						target_point.has_emitted = true
+						#target_point = inter
+						#target_point.has_emitted = true
+						if (inter.position - target_point.position).length() > limit:
+							target_point = inter
+							target_point.has_emitted = true
+						else:
+							p_end = target_point
+						
+						
+						
 				var k: Vector2i = chunks.find_key(chunk) 
 				var neighbor_keys: Array[Vector2i] = get_neighbor_keys(k)
 				for neighbor_key: Vector2i in neighbor_keys:
@@ -109,100 +142,38 @@ func expand_structure() -> void:
 						var neighbor_chunk: Chunk3D = chunks[neighbor_key]
 						for line: CellLine in neighbor_chunk.get_lines():
 							var inter: CellPoint =\
-								line_intersection(
-									p_end.position,
-									target_point.position,
-									line.start.position,
-									line.end.position
+								CellStructureUtils.line_intersection(
+									p_end,
+									target_point,
+									line.start,
+									line.end
 								)
 							if inter != null:
 								target_point = inter
 								target_point.has_emitted = true
 
 				for line: CellLine in new_lines:
-					var inter: CellPoint = line_intersection(p_end.position, target_point.position, line.start.position, line.end.position)
+					var inter: CellPoint =\
+						CellStructureUtils.line_intersection(
+							p_end, 
+							target_point, 
+							line.start, 
+							line.end
+						)
 					if inter != null:
 						#target_point = line.end
 						target_point = inter
 						target_point.has_emitted = true
-
 				var new_line: CellLine = CellLine.new(p_end, target_point)
 				new_lines.append(new_line)
+				#if (p_end.position - target_point.position).length() > limit:
+					#var new_line: CellLine = CellLine.new(p_end, target_point)
+					#new_lines.append(new_line)
+				#else:
+					#p_end = target_point
 
 		for line: CellLine in new_lines:
 			var target_chunk: Chunk3D = get_chunk_for_point(line.start.position)
 			if target_chunk != null:
 				need_expand = true
 				target_chunk.add_line(line)
-
-func calculate_angle(start: Vector2, end: Vector2) -> float:
-	# Вычисление угла в радианах
-	var dx: float = end.x - start.x
-	var dy: float = end.y - start.y
-	return atan2(dy, dx)
-
-func generate_child_rays(start_point: CellPoint, base_direction: float, child_count: int = 2,
-						min_length: float = 1, max_length: float = 2,
-						max_deviation: float = PI) -> Array[CellPoint]:
-	"""
-	Генерирует child_count лучей из start_point.
-	Направление каждого луча основывается на base_direction (в радианах) с отклонением не более max_deviation.
-
-	Возвращает список лучей, где каждый луч представлен как список [start_point, end_point],
-	а end_point – объект CellPoint с целочисленными координатами.
-	"""
-	if child_count == 0:
-		return []
-	var points: Array[CellPoint] = []
-	var rng: RandomNumberGenerator = RandomNumberGenerator.new()
-	var base_step: float = max_deviation * 2 / child_count
-	for n: int in range(child_count):
-		# Вычисляем случайное отклонение
-		var deviation: float = rng.randf_range(-max_deviation + base_step * n, -max_deviation + base_step * (n+1))
-		var new_angle: float = base_direction + deviation
-		# Выбираем случайную длину луча
-		var length: float = rng.randf_range(min_length, max_length)
-		var dx: float  = cos(new_angle) * length
-		var dy: float = sin(new_angle) * length
-		# Вычисляем координаты конечной точки и округляем до целых чисел
-		var new_x: float = start_point.position.x + dx
-		var new_y: float = start_point.position.y + dy
-		var end_point: CellPoint = CellPoint.new(Vector2(new_x, new_y))
-		points.append(end_point)
-	return points
-
-
-func line_intersection(p1: Vector2, p2: Vector2, p3: Vector2, p4: Vector2, tol: float = 1e-6) -> CellPoint:
-	"""
-	Вычисляет точку пересечения двух отрезков (p1, p2) и (p3, p4) с учетом допуска tol.
-	p1, p2, p3, p4 – объекты Vector2, представляющие концы отрезков.
-	Возвращает Vector2 с координатами точки пересечения, если отрезки пересекаются, иначе null.
-	"""
-	if abs(p1.x - p4.x) < 1e-6 and abs(p1.y - p4.y) < 1e-6:
-		return null  # Новый отрезок исходит из этой прямой
-	if abs(p1.x - p3.x) < 1e-6 and abs(p1.y - p3.y) < 1e-6:
-		return null  # Новый отрезок исходит из этой прямой
-		
-	# Направления отрезков
-	var d1: Vector2 = p2 - p1
-	var d2: Vector2 = p4 - p3
-		
-	# Определитель
-	var det: float = d1.x * d2.y - d1.y * d2.x
-	# Если определитель близок к нулю, отрезки параллельны или совпадают
-	if abs(det) < tol:
-		return null
-	# Вычисление параметра t для точки пересечения
-	var diff: Vector2 = p3 - p1
-	var t: float = (diff.x * d2.y - diff.y * d2.x) / det
-	# Проверка, что точка пересечения находится на первом отрезке
-	if t < 0 or t > 1:
-		return null
-	# Вычисление параметра u для точки пересечения
-	var u: float = (diff.x * d1.y - diff.y * d1.x) / det
-	# Проверка, что точка пересечения находится на втором отрезке
-	if u < 0 or u > 1:
-		return null
-	# Вычисление координат точки пересечения
-	var intersection: Vector2 = p1 + t * d1
-	return CellPoint.new(intersection)
